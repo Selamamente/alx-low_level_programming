9adf41f21e4c81ee4721a93ce3e0d0ecad6b878e
selamamente <selampostu@gmail.com>
2022-06-04 15:32:54 -0700

    task six commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,9 @@
 #include <stdio.h>
 
-/*
- *
- * main - Entry point
- *
- * Return: Always 0 (Success)
- *
+/**
+* main - Entry point
+* Description: c is good
+* Return: Always 0 (Success)
 */
 
 int main(void)
