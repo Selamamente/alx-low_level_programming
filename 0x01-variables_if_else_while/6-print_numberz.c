89a7aba110a53527d54a17d5e1732d27f294737b
selamamente <selampostu@gmail.com>
2022-06-06 10:12:57 -0700

    task six commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - prints all single digit number of base 10 strating from 0
+* Description: NumberZ
+* Return: return 0 (success)
+*/
+
+int main(void)
+{
+
+int n;
+
+for (n = 0; n < 10; n++)
+{
+printf("%d", n);
+}
+printf("\n");
+return (0);
+}
