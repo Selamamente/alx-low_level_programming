6b6feea3e4f1af8bde0c49de9ae25568ac603a0b
selamamente <selampostu@gmail.com>
2022-06-06 10:37:01 -0700

    task seven commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - prints the lowwer alphabets in reverse
+* Description: smile in the mirror
+* Return: return 0 (success)
+*/
+int main(void)
+{
+int n;
+for (n = 'z'; n >= 'a'; n--)
+{
+putchar(n);
+}
+putchar(10);
+return (0);
+}
