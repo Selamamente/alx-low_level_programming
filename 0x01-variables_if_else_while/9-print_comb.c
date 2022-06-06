db711a5fa40dcc049ef74aef636de2e0e18d0407
selamamente <selampostu@gmail.com>
2022-06-06 13:53:47 -0700

    task nine commit

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,4 +1,25 @@
-#include <stdio>
+#include <stdio.h>
 
 /**
-* main - prints all possible combinations of single-digit numbers 
+* main - prints all possible combinations of single-digit numbers
+* Description: an unbeatable combination for success
+* Return: return 0 (success)
+*/
+
+int main(void)
+{
+
+
+int n;
+for (n = 48; n <= 57; n++)
+{
+putchar(n);
+if (n != 57)
+{
+putchar(44);
+putchar(32);
+}
+}
+putchar(10);
+return (0);
+}
