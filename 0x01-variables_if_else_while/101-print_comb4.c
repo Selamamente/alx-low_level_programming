dd82fd1eb17bc78dc5853fba8bb7db16ec6af1cc
selamamente <selampostu@gmail.com>
2022-06-06 12:57:18 -0700

    task eleven commit

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+* main - prints all possible different combinations of three digits
+* Description: The success combination in business
+* Return: return 0 (success)
+*/
+
+int main(void)
+{
+
+int n;
+int i;
+int j;
+
+for (n = 48; n < 58; n++)
+{
+for (i = 49; i < 58; i++)
+{
+for (j = 50; j < 58; j++)
+{
+if (j > i && i > n)
+{
+putchar(n);
+putchar(i);
+putchar(j);
+if (n != 55 || j != 56)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
