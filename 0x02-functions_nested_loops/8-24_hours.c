d790b2d65cba9ccdeb68449371a2639f6aa7f52d
selamamente <selampostu@gmail.com>
2022-06-07 16:56:39 -0700

    task eight commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,7 +10,7 @@ void jack_bauer(void)
 
 int i, j;
 
-for (i = 0 i < 24; i++)
+for (i = 0; i < 24; i++)
 {
 
 for (j = 0; j < 60; j++)
@@ -25,7 +25,7 @@ _putchar(i + '0');
 else if (i >= 10)
 {
 _putchar((i / 10) + '0');
-_putchar((i % 10) + '0'0);
+_putchar((i % 10) + '0');
 }
 if (j < 10)
 {
