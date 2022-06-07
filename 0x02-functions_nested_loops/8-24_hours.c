f02f51ed3e19571218b5fef8a07d679d248d0824
selamamente <selampostu@gmail.com>
2022-06-07 16:52:33 -0700

    task eight commit

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+* jack_bauer - prints every minute of the day of Jack Bauer,from 00:00-23:59
+*/
+
+
+void jack_bauer(void)
+{
+
+int i, j;
+
+for (i = 0 i < 24; i++)
+{
+
+for (j = 0; j < 60; j++)
+{
+
+if (i < 10)
+{
+
+_putchar('0');
+_putchar(i + '0');
+}
+else if (i >= 10)
+{
+_putchar((i / 10) + '0');
+_putchar((i % 10) + '0'0);
+}
+if (j < 10)
+{
+_putchar(':');
+_putchar('0');
+_putchar(j + '0');
+}
+else if (j >= 10)
+{
+_putchar(':');
+_putchar((j / 10) + '0');
+_putchar((j % 10) + '0');
+}
+_putchar('\n');
+}
+}
+}
