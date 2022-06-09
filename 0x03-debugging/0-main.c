574b01c67a62c972de7fc2428621bcb0e13ea213
selamamente <selampostu@gmail.com>
2022-06-09 13:27:43 -0700

    task zero commit

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* main - test that the function positive_or_negative
+* Description: Multiple mains
+* Return: return 0 for success
+*
+*/
+
+int main(void)
+{
+
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+return (0);
+}
