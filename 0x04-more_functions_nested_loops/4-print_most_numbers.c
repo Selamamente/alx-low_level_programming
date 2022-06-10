526f3f210ae45fd4b20c2f9a12186fa59066f509
selamamente <selampostu@gmail.com>
2022-06-10 08:35:14 -0700

    task four commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,19 +2,16 @@
 
 /**
 * print_most_numbers - prints the numbers, from 0 to 9
-*
-*
 */
 
 void print_most_numbers(void)
 {
-int i;
+	int i;
 
-for (i = 0; i < 10; i++)
-{
-
-	if (i != 2 && i != 4)
+	for (i = 0; i < 10; i++)
+	{
+		if (i != 2 && i != 4)
 		_putchar(x + '0');
-}
+	}
 	_putchar('\n');
 }
