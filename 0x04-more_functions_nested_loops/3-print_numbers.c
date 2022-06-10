4eae6448a37a703d1d00c5e18782501d0914b989
selamamente <selampostu@gmail.com>
2022-06-09 18:01:12 -0700

    task three commit

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+* print_numbers - prints the numbers, from 0 to 9, followed by a new line
+* Description: the numbers speak for themselves
+*
+*
+*/
+
+
+
+void print_numbers(void)
+{
+
+int i;
+for (i = 0; x < 10; x++)
+	_putchar(i + '0');
+	_putchar('\n');
+
+}
