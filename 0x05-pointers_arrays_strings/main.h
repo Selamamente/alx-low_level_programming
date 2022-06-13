ac5139f454d99f1f45915bb78108a025fd6d498c
selamamente <selampostu@gmail.com>
2022-06-13 13:20:32 -0700

    task one commit

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+void reset_to_98(int *n);
+int _putchar(char c);
+void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+#endif
