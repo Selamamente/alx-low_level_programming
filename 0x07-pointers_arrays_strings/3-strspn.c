61b86bde53a87bc47031ad02c1efeefef7dc899b
selamamente <selampostu@gmail.com>
2022-06-20 13:46:55 -0700

    task three commit

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+* _strspn - prints a function that gets the length of a prefix
+* @s: parameter with string
+* @accept: parameter with char
+* Return: number of bytes in the init segment
+*
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+unsigned int i = 0, j = 0, k = 0;
+
+while (accept[i])
+{
+
+while (s[j] != 32)
+{
+if (accept[i] == s[j])
+{
+k++;
+}
+j++;
+}
+i++;
+}
+return (k);
+}
