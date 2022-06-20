ac03b5d2da8db43f0af0e5893027eac11ce42bac
selamamente <selampostu@gmail.com>
2022-06-20 14:09:36 -0700

    task four commit

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+* _strpbrk - prints a function searches a string for any of a set of byte
+* @s: parameter with string
+* @accept: accepted characters
+* Return: return the first found accepted character
+*
+*/
+
+char *_strpbrk(char *s, char *accept)
+{
+
+int i = 0, j;
+
+while (s[i])
+{
+j = 0;
+while (accept[j])
+{
+
+	if (s[i] == accept[j])
+	{
+	s += i;
+	return (s);
+	}
+j++;
+}
+i++;
+}
+return ('\0');
+}
