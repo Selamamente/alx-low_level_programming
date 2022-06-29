ee229f8f3a92d1a8d6d09d04c51de8f0995e9b64
selamamente <selampostu@gmail.com>
2022-06-29 01:51:35 -0700

    task one commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - prints a fuctionn its name
+* @argc: argc parameter
+* @argv: an array of a command list
+* Return: 0 for succes
+*
+*/
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+
+printf("%s\n", *argv);
+return (0);
+
+}
