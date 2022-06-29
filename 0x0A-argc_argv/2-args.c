ee90692dc14b465849453dacd7a0a805c4434b92
selamamente <selampostu@gmail.com>
2022-06-29 15:14:03 -0700

    task two commit

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - prints its name
+* @argc: parameter
+* @argv: an array of acommand
+* Return: returns 0 for success
+*
+*/
+
+int main(int argc, char *argv[])
+{
+
+int i;
+	for (i = 0; i < argc; i++)
+	{
+	
+	printf("%s\n", argv[i]);
+
+	}
+return (0);
+}
